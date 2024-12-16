95e31f6bab110fbd837e945b43769c4cd9e32db6
harie-hunt <haripuji8@gmail.com>
2024-12-16 08:31:28 +0700

    trim zod

--- src/lib/schema/auth.ts
+++ src/lib/schema/auth.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-    username: z.string().min(4),
-    password: z.string().min(4),
+    username: z.string().min(4).trim(),
+    password: z.string().min(4).trim(),
 })
 
 export const registerSchema = loginSchema.merge(z.object({
-    confirmPassword: z.string()
+    confirmPassword: z.string().trim()
 })).superRefine((v, c) => {
     if (v.password != v.confirmPassword) c.addIssue({
         message: 'Confirm Password dan Password tidak sama !',
--- src/lib/schema/transaksi.ts
+++ src/lib/schema/transaksi.ts
@@ -4,7 +4,7 @@ import id from "./id";
 export const transaksiCreateSchema = z.object({
     tanggal: z.string().pipe(z.coerce.date()),
     anggaran_id: z.string().uuid(),
-    uraian: z.string().min(1),
+    uraian: z.string().min(1).trim(),
     nilai: z.string().pipe(z.coerce.number().min(1)),
 })
 
